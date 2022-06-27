fb0e010129c2bfcbc440db85abc241264997594e
pouyam <pouyamohammadyirbu@gmail.com>
2022-06-27 07:51:05 -0700

    debug: handling exceptions

--- data-extractor/crawler.py
+++ data-extractor/crawler.py
@@ -1,6 +1,7 @@
 # Importing required modules
-from asyncio.windows_events import NULL
-from telethon.sync import TelegramClient
+# from telethon.sync import TelegramClient
+import sys
+from telethon import TelegramClient
 import configparser
 import datetime
 import json
@@ -14,9 +15,13 @@ app.config["DEBUG"] = True
 
 def read_conf():
     # reading config file
-    config = configparser.ConfigParser()
-    config.read("sys.ini")
-    return config
+    try:
+        config = configparser.ConfigParser()
+        config.read("sys.ini")
+        return config
+    except:
+        print("Failed to read sys.ini files")
+        return False
 
 # this method saves a json file
 def save_json(data_list, file_address):
@@ -26,6 +31,7 @@ def save_json(data_list, file_address):
             json.dump(data_list, f ,ensure_ascii=False, indent=4)
         return True
     except:
+        print("Cannot save json file")
         return False
 
 # this method modifies sys.ini
@@ -33,10 +39,21 @@ def modify_conf(limit=-1, date=-1):
     if limit < 0 and date == -1:
         return
     config = read_conf()
+    if config is False:
+        print("Cannot modify 'sys.ini'. reason: cannot read config!")
+        return
     if limit != -1:
-        config.set('setting', 'limit', limit)
+        config.set('setting', 'limit', str(limit))
     if date != -1:
-        config.set('setting', 'date', date)
+        config.set('setting', 'date', str(date))
+    try:
+        with open('sys.ini', 'w') as configfile:
+            config.write(configfile)
+    except:
+        print("Cannot modify 'sys.ini'. reason: cannot write file")
+        print("Detail: ",  sys.exc_info()[0], "occured")
+        return
+
 
 # crawler method
 @app.route('/crawl', methods=['GET'])
@@ -44,6 +61,10 @@ def crawl():
 
     # reading config files
     config = read_conf()
+    if config is False:
+        print("Cannot crawl! reason: cannot read config info")
+        return jsonify({"attempt" : "failed", "reason" : "cannot read sys.ini"})
+
     # client configs
     api_id = config["client"]["api_id"]
     api_hash = config["client"]["api_hash"]
@@ -52,59 +73,62 @@ def crawl():
     # database configs
     file_address = config["database"]["file_address"]
     # extractor config
-    limit = config["setting"]["limit"]
+    limit = int(config["setting"]["limit"])
     date = config["setting"]["date"]
-
-    if date == 0:
+    if date is '0':
         date = datetime.date.today()
 
     # exisiting channel info
     channeles = []
     channeles_name = {}
 
-    # gathering channel infos
-    with TelegramClient(username, api_id, api_hash) as client:
-        for dialog in client.iter_dialogs():
-            if dialog.is_group or dialog.is_channel:
-                # print("Channel ID: " + str(dialog.id) + ", Channel Title: " + dialog.title)
-                channeles.append(dialog.id)
-                channeles_name['{}'.format(dialog.id)] = '{}'.format(dialog.title)
+    try:
+        # gathering channel infos
+        with TelegramClient(username, api_id, api_hash) as client:
+            for dialog in client.iter_dialogs():
+                if dialog.is_group or dialog.is_channel:
+                    # print("Channel ID: " + str(dialog.id) + ", Channel Title: " + dialog.title)
+                    channeles.append(dialog.id)
+                    channeles_name['{}'.format(dialog.id)] = '{}'.format(dialog.title)
 
-        # gathered data will be appended to this list
-        data_list = []
+            # gathered data will be appended to this list
+            data_list = []
 
-        # iterating over today messages of channels 
+            # iterating over today messages of channels 
 
-        counter = 0
-        for chat in channeles:    
-            for message in client.iter_messages(chat, offset_date=date , reverse=True):
+            counter = 0
+            for chat in channeles:    
+                for message in client.iter_messages(chat, offset_date=date , reverse=True):
 
-                # data format
-                data = {
-                "unique_id" : (str(chat) + '*' + str(message.id)),      # str
-                "channel_id" : chat,                                    # int
-                "channel_name" : channeles_name['{}'.format(chat)],     # str
-                "id" : message.id,                                      # int
-                "from_id" : str(message.from_id),                       # str or null
-                "text" : message.text,                                  # str
-                "date" : str(message.date),                             # str
-                "views" : message.views,                                # int
-                "forwards" : message.forwards,                          # int
-                "edit_date" : str(message.edit_date)                    # str
-                }
-                # print(data)    
-                data_list.append(data)
-                
-                counter = counter + 1
-                if limit > 0:
-                    if counter >= limit:
-                        break
+                    # data format
+                    data = {
+                    "unique_id" : (str(chat) + '*' + str(message.id)),      # str
+                    "channel_id" : chat,                                    # int
+                    "channel_name" : channeles_name['{}'.format(chat)],     # str
+                    "id" : message.id,                                      # int
+                    "from_id" : str(message.from_id),                       # str or null
+                    "text" : message.text,                                  # str
+                    "date" : str(message.date),                             # str
+                    "views" : message.views,                                # int
+                    "forwards" : message.forwards,                          # int
+                    "edit_date" : str(message.edit_date)                    # str
+                    }
+                    # print(data)    
+                    data_list.append(data)
+                    
+                    counter = counter + 1
+                    if limit > 0:
+                        if counter >= limit:
+                            break
 
-        # saving json file
-        save_json(data_list, file_address)
+            # saving json file
+            save_json(data_list, file_address)
 
-        # returning jsonified result
-        return jsonify(data_list)
+            # returning jsonified result
+            return jsonify(data_list)
+    except:
+        print("Cannot crawl data! ", sys.exc_info()[0], " occured!")
+        return jsonify({"attempt" : "failed", "reason" : "not defined!"})    
 
 # call this method to crawl telegram messages
 
